73cd0d972d703ae32880c8267805cb91541f813b
Nicolas Leon <nicolasaleon05@gmail.com>
2025-05-25 21:40:42 -0300

    Add plataform movement

---
+++ Assets/Scripts/PlataformMovement.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+//[RequireComponent(typeof(Rigidbody))]
+public class PlataformMovement : MonoBehaviour
+{
+    enum MovementType
+    {
+        Vertical,
+        HorizontalX,
+        HorizontalZ
+    }
+
+    [SerializeField] private float totalMovement = 2f;
+    [SerializeField] private float movementSpeed = 1f;
+    [SerializeField] private MovementType movementType;
+
+    private Vector3 startPos;
+    private Vector3 targetOffset;
+    private bool goingForward = true;
+
+    private void Start()
+    {
+        startPos = transform.position;
+
+        switch (movementType)
+        {
+            case MovementType.Vertical:
+                targetOffset = new Vector3(0, totalMovement, 0);
+                break;
+            case MovementType.HorizontalX:
+                targetOffset = new Vector3(totalMovement, 0, 0);
+                break;
+            case MovementType.HorizontalZ:
+                targetOffset = new Vector3(0, 0, totalMovement);
+                break;
+        }
+    }
+
+    private void Update()
+    {
+        Vector3 targetPos = goingForward ? startPos + targetOffset : startPos;
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, movementSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, targetPos) < 0.01f)
+        {
+            goingForward = !goingForward;
+        }
+    }
+
+}
