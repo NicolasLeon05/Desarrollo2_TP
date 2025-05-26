3539050a2197e187775736fde717b315f2c52f3d
Nicolas Leon <nicolasaleon05@gmail.com>
2025-05-26 08:41:12 -0300

    Add pauseController (not working)

---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PauseController : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private InputActionReference pauseAction;
+
+    [SerializeField] private bool isPaused = false;
+
+    private void OnEnable()
+    {
+        if (pauseAction != null)
+        {
+            pauseAction.action.performed += OnPause;
+        }
+    }
+
+    private void Awake()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    private void OnPause(InputAction.CallbackContext context)
+    {
+        TogglePause();
+    }
+
+    private void TogglePause()
+    {
+        isPaused = !isPaused;
+
+        pauseMenu.SetActive(isPaused);
+
+        if (isPaused)
+            Time.timeScale = 0f;
+        else
+            Time.timeScale = 1f;
+    }
+
+}
