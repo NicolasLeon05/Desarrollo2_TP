7241708b9fb3019d03c2f8112d066afc3b35b959
Nicolas Leon <nicolasaleon05@gmail.com>
2025-07-04 11:37:21 -0300

    Merge al menues in one scene (pause incomplete)

--- Assets/Scripts/SceneNavigation/NavigationController.cs
+++ Assets/Scripts/SceneNavigation/NavigationController.cs
@@ -8,6 +8,14 @@ public class NavigationController : MonoBehaviour //SelectionKeeper
     private EventSystem eventSystem;
     private GameObject lastSelectedOption;
 
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject creditsMenu;
+    [SerializeField] private GameObject pauseMenu;
+
+    [SerializeField] private GameObject firstMainMenuButton;
+    [SerializeField] private GameObject firstCreditsMenuButton;
+    [SerializeField] private GameObject firstPauseMenuButton;
+
     [SerializeField] private InputActionReference navigateAction;
     private Vector2 navigateInput = Vector2.zero;
 
@@ -18,6 +26,12 @@ public class NavigationController : MonoBehaviour //SelectionKeeper
         lastSelectedOption = eventSystem.firstSelectedGameObject;
     }
 
+    private void Start()
+    {
+        mainMenu.SetActive(true);
+        creditsMenu.SetActive(false);
+        pauseMenu.SetActive(false);  
+    }
 
     private void OnEnable()
     {
@@ -55,11 +69,40 @@ public class NavigationController : MonoBehaviour //SelectionKeeper
     private void OnNavigate(InputAction.CallbackContext obj)
     {
         navigateInput = obj.ReadValue<Vector2>();
-
     }
 
     private bool WasNavigatePressed()
     {
         return navigateInput != Vector2.zero;
+    }
+
+    public void SetMainMenuActive()
+    {
+        mainMenu.SetActive(true);
+        creditsMenu.SetActive(false);
+        pauseMenu.SetActive(false);
+
+        eventSystem.SetSelectedGameObject(firstMainMenuButton);
+        lastSelectedOption = firstMainMenuButton;
+    }
+
+    public void SetCreditsActive()
+    {
+        mainMenu.SetActive(false);
+        creditsMenu.SetActive(true);
+        pauseMenu.SetActive(false);
+
+        eventSystem.SetSelectedGameObject(firstCreditsMenuButton);
+        lastSelectedOption = firstCreditsMenuButton;
+    }
+
+    public void SetPauseActive()
+    {
+        mainMenu.SetActive(false);
+        creditsMenu.SetActive(false);
+        pauseMenu.SetActive(true);
+
+        eventSystem.SetSelectedGameObject(firstPauseMenuButton);
+        lastSelectedOption = firstPauseMenuButton;
     }
 }
