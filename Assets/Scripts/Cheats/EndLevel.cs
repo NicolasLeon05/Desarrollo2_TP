68fc2cd20313bb06778406ece4c10e124a2aa2b6
Nicolas Leon <nicolasaleon05@gmail.com>
2025-07-07 22:19:19 -0300

    Add signs to Level1 tutorial texts

--- Assets/Scripts/Cheats/EndLevel.cs
+++ Assets/Scripts/Cheats/EndLevel.cs
@@ -4,6 +4,7 @@ public class EndLevel : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
+        SoundManager.Instance.PlaySound(SoundType.UnlockLevel);
         if (other.CompareTag("Player"))
         {
             SceneController.Instance.LoadNextAdditive();
